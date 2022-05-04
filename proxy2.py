e1d7c3bafe9c11ab1db20014c81585deac6ace9b
Morozov <libertariansessions@gmail.com>
2022-05-04 14:57:40 +0500

    minor changes

--- proxy2.py
+++ proxy2.py
@@ -1,65 +1,66 @@
-import socket
-import time
-import ssl
+import socket #кто понял что происходит в програме тово диференцырут
 import threading
 import typing
 
-def do(server:socket.socket):
+packet_size = 6400
+timeout = 1
+
+
+def https(headers, browser):
+    client = socket.socket()
+    try:
+        client.connect((headers.split()[1].split("/")[0].split(":")[0], 443))
+        reply = "HTTP/1.1 200 Connection established\r\n"
+        reply += "Proxy-agent: Dota 2\r\n"
+        reply += "\r\n"
+        browser.sendall(reply.encode())
+    except socket.error as err:
+        pass
+
+    browser.setblocking(True)
+    browser.settimeout(timeout)
+    client.setblocking(True)
+    client.settimeout(timeout)
     while True:
-        browser, addr = server.accept()
-        browser.settimeout(5)
-        recieved_from_browser = browser.recv(1024)
-        print(recieved_from_browser)
+        try:
+            request = browser.recv(packet_size)
+            client.sendall(request)
+        except Exception as err:
+            pass
+        try:
+            reply = client.recv(packet_size)
+            browser.sendall(reply)
+        except Exception as err:
+            pass
+
+def http(recieved_from_browser, browser):
+    client = socket.socket()
+    client.settimeout(timeout)
+    dec_req = recieved_from_browser.decode().split()[1]
+    host, port = dec_req.split('/')[2], 80
+    client.connect((host, port))
+    while True:
+        client.sendall(recieved_from_browser)
+        rec = client.recv(packet_size)
+        browser.send(rec)
+
+def do(upper_socket : socket.socket):
+    while True:
+        browser, addr = upper_socket.accept()
+        browser.settimeout(timeout)
+        recieved_from_browser = browser.recv(packet_size)
         if not recieved_from_browser:
             continue
         headers = recieved_from_browser.decode()
         if "CONNECT" in headers:
-            client = socket.socket()
-            # Connect to port 443
-            try:
-                # If successful, send 200 code response
-                print(headers)
-                print(headers.split()[1].split("/")[0].split(":")[0])
-                client.connect((headers.split()[1].split("/")[0].split(":")[0], 443))
-                reply = "HTTP/1.1 200 Connection established\r\n"
-                reply += "Proxy-agent: Pyx\r\n"
-                reply += "\r\n"
-                browser.sendall(reply.encode())
-            except socket.error as err:
-                # If the connection could not be established, exit
-                # Should properly handle the exit with http error code here
-                print(err)
-                break
-
-            # Indiscriminately forward bytes
-            browser.setblocking(False)
-            client.setblocking(False)
-            print("go in wt")
-            while True:
-                try:
-                    request = browser.recv(1024)
-                    client.sendall(request)
-                except Exception as err:
-                    pass
-                try:
-                    reply = client.recv(1024)
-                    browser.sendall(reply)
-                except Exception as err:
-                    pass
+            https(headers, browser)
         else:
-            client = socket.socket()
-            dec_req = recieved_from_browser.decode().split()[1]
-            host, port = dec_req.split('/')[2], 80
-            client.connect((host, port))
-            while True:
-                client.sendall(recieved_from_browser)
-                rec = client.recv(1024)
-                browser.send(rec)
+            http(recieved_from_browser, browser)
 
 
 server = socket.socket()
-server.bind(('localhost', 8080))
+server.bind(('localhost', 8081))
 server.listen(100)
 while True:
-    thread=threading.Thread(target=do, args=(server,))
+    thread = threading.Thread(target=do, args=(server,))
     thread.start()
