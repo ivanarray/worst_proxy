7154c66c590d10a35709edf798679e4ee351a3d9
ivanarray <i.maliamow@yandex.ru>
2022-05-10 22:15:02 +0500

    сделал тесты, переделал логику работы с хостом и удаленным сервером на разные потоки

--- proxy_tests.py
+++ proxy_tests.py
@@ -1,8 +1,18 @@
 import socket
+import threading
 import unittest.mock
+import socketserver
+
 from threading import Thread
 
 import server_handler
+
+
+class EchoHandler(socketserver.BaseRequestHandler):
+    def handle(self) -> None:
+        self.request.settimeout(server_handler.HttpProxyHandler.TIMEOUT)
+        mes: bytes = self.request.recv(server_handler.HttpProxyHandler.PACKET_SIZE)
+        self.request.sendall(mes)
 
 
 class ProxyShould(unittest.TestCase):
@@ -10,10 +20,8 @@ class ProxyShould(unittest.TestCase):
                     b'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0\r\n' \
                     b'Proxy-Connection: keep-alive\r\nConnection: keep-alive\r\nHost: localhost:443\r\n\r\n'
 
-    HTTPS_REQ = 1
-
     HTTP_REQ = b'GET http://somkural.ru/ HTTP/1.1\r\n' \
-               b'Host: localhost:80\r\n' \
+               b'Host: localhost:8084\r\n' \
                b'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0\r\n' \
                b'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n' \
                b'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3\r\n' \
@@ -22,37 +30,54 @@ class ProxyShould(unittest.TestCase):
                b'Upgrade-Insecure-Requests: 1\r\n\r\n'
 
     def setUp(self) -> None:
-        self.serv = server.HttpProxyHandler(8081, 'localhost', 0.5)
-        Thread(target=self.serv.run).start()
-        self.http_server = socket.socket()
-        self.https_server = socket.socket()
-        self.http_server.bind(('localhost', 80))
-        self.https_server.bind(('localhost', 443))
-        self.https_server.listen(100)
-        self.http_server.listen(1)
+        self.proxy = socketserver.TCPServer(('localhost', 8081), server_handler.HttpProxyHandler)
+        self.http_server = socketserver.TCPServer(('localhost', 8084), EchoHandler)
+        self.https_server = socketserver.TCPServer(('localhost', 443), EchoHandler)
+        threading.Thread(target=self.proxy.serve_forever).start()
+        threading.Thread(target=self.https_server.serve_forever).start()
+        threading.Thread(target=self.http_server.serve_forever).start()
 
     def tearDown(self) -> None:
-        self.https_server.close()
-        self.http_server.close()
+        self.proxy.shutdown()
+        self.https_server.shutdown()
+        self.http_server.shutdown()
+        self.proxy.server_close()
+        self.https_server.server_close()
+        self.http_server.server_close()
 
     def test_when_https_should_return_connect_message(self):
-        self.https_server.listen(1)
         host = socket.socket()
+        host.settimeout(server_handler.HttpProxyHandler.TIMEOUT)
         host.connect(('localhost', 8081))
         host.send(self.HTTPS_CONNECT)
-        ans = host.recv(server.HttpProxyHandler.PACKET_SIZE)
-        self.assertTrue("connection established" in ans.decode().lower())
+        ans = host.recv(server_handler.HttpProxyHandler.PACKET_SIZE)
+        host.send(b'')
+        host.close()
+
+        self.assertTrue("200" in ans.decode().lower())
 
     def test_when_https_should_to_transmitted_messages_after_connect(self):
         host = socket.socket()
+        host.settimeout(server_handler.HttpProxyHandler.TIMEOUT)
         host.connect(('localhost', 8081))
-        host.send(self.HTTPS_CONNECT)
-        self.https_server.accept()
-        ans = host.recv(server.HttpProxyHandler.PACKET_SIZE)
-        host.send("12345".encode())
-        res = self.https_server.recv(server.HttpProxyHandler.PACKET_SIZE)
 
-        self.assertTrue(res.decode() == "12345")
+        host.send(self.HTTPS_CONNECT)
+        host.recv(1024)
+        host.send(b'12345')
+        res = host.recv(server_handler.HttpProxyHandler.PACKET_SIZE)
+        host.close()
+
+        self.assertTrue(res.decode() == '12345')
+
+    def test_http_server_should(self):
+        host = socket.socket()
+        host.settimeout(server_handler.HttpProxyHandler.TIMEOUT)
+        host.connect(('localhost', 8081))
+        host.send(self.HTTP_REQ)
+        answer = host.recv(60000)
+        host.close()
+
+        self.assertEqual(self.HTTP_REQ, answer)
 
 
 if __name__ == '__main__':
--- server_handler.py
+++ server_handler.py
@@ -1,19 +1,21 @@
 import re
 import socket
 import socketserver
+import threading
 
+import server_handler
 from connection_type import ConnectionType
 
 
 class HttpProxyHandler(socketserver.BaseRequestHandler):
-    CONNECTION_REPLY = "HTTP/1.1 200 Connection established\r\n\r\n".encode()
+    CONNECTION_REPLY = b'HTTP/1.1 200 Connection established\r\n\r\n'
     LINK_REG = re.compile(r'(?<=Host: )(?P<name>[^\n:\r ]+)(:(?P<port>\d+))?')
     PACKET_SIZE = 65534
-    TIMEOUT = 2
+    TIMEOUT = 0.5
 
     def handle(self) -> None:
         try:
-            print(f"connect to {self.client_address}")
+            print(f'connect to {self.client_address}')
             self.request.settimeout(self.TIMEOUT)
             data: bytes = self.request.recv(self.PACKET_SIZE)
             d_data = data.decode()
@@ -27,57 +29,56 @@ class HttpProxyHandler(socketserver.BaseRequestHandler):
                 if port is None:
                     port = 80 if connect_type is connect_type.HTTP else 443
                 sock.connect((name, int(port)))
-                if connect_type is connect_type.HTTP:
-                    self.handle_http(sock, data)
+                if connect_type is connect_type.HTTPS:
+                    self.request.sendall(self.CONNECTION_REPLY)
                 else:
-                    self.handle_https(sock, data)
+                    sock.sendall(data)
+                a = threading.Thread(target=server_handler.HttpProxyHandler.handle_from_remote, args=(self, sock))
+                a.start()
+                with sock.dup() as sock_d:
+                    self.handle_from_client(sock_d)
         except Exception as e:
             print(e)
 
     def finish(self) -> None:
         self.request.close()
 
-    def handle_https(self, remote_server: socket.socket, data: bytes):
-        self.request.sendall(self.CONNECTION_REPLY)
+    def handle_from_remote(self, remote_server: socket.socket):
+        errors_count = 0
         while True:
-            try:
-                try:
-                    data = self.request.recv(self.PACKET_SIZE)
-                    remote_server.sendall(data)
-                except socket.error:
-                    pass
-                try:
-                    rec = remote_server.recv(self.PACKET_SIZE)
-                    self.request.sendall(rec)
-                    if len(rec) < 1 or len(data) < 1:
-                        break
-                except socket.error:
-                    pass
-            except Exception as e:
-                print(e)
+            if errors_count > 5:
                 break
+            try:
+                rec = remote_server.recv(self.PACKET_SIZE)
+                self.request.sendall(rec)
+                if len(rec) < 1:
+                    break
+                errors_count = 0
+            except socket.timeout:
+                errors_count += 1
+                continue
+                pass
+            except Exception as e:
+                print(f'{type(e)}----{e}')
+                return
 
-    def handle_http(self, remote_server: socket.socket, data: bytes):
-        remote_server.sendall(data)
+    def handle_from_client(self, remote_server: socket.socket):
+        errors_count = 0
         while True:
-            try:
-                try:
-                    rec = remote_server.recv(self.PACKET_SIZE)
-                    self.request.sendall(rec)
-                    if len(rec) < 1:
-                        break
-                except socket.timeout:
-                    pass
-                try:
-                    data = self.request.recv(self.PACKET_SIZE)
-                    remote_server.sendall(data)
-                    if len(data) < 1:
-                        break
-                except socket.timeout:
-                    pass
-            except Exception as e:
-                print(e)
+            if errors_count > 5:
                 break
+            try:
+                data = self.request.recv(self.PACKET_SIZE)
+                remote_server.sendall(data)
+                if len(data) < 1:
+                    break
+                errors_count = 0
+            except socket.timeout:
+                errors_count += 1
+                pass
+            except Exception as e:
+                print(f'{type(e)}----{e}')
+                return
 
     def parse_address(self, page: str) -> dict:
         return self.LINK_REG.search(page).groupdict()
